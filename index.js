c311af3506eac2b379ce389421b889060f3d4051
DikDns <dikdnssocial@gmail.com>
2022-05-22 11:22:50 +0700

    included rest and routes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types");
 const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
